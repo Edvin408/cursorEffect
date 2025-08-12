0a32aa3baad30ddcb599ca3320ad7e7390dc51c2
Edvin <liuhello0@gmail.com>
2025-08-12 10:41:22 +0100

    sync

--- install/build.sh
+++ install/build.sh
@@ -3,5 +3,6 @@ mkdir -p ~/.config/cursorEffect
 if [ ! -f ~/.config/cursorEffect/config.h ]; then
     wget -P ~/.config/cursorEffect https://raw.githubusercontent.com/Edvin408/cursorEffect/refs/heads/main/config.h
 fi
+cd ~/.config/cursorEffect 
 gcc /opt/cursorEffect/main.c -o /tmp/cursorEffects -lX11 -lXrender -lXcomposite -lXext -lXi -lm
 /tmp/cursorEffects &
